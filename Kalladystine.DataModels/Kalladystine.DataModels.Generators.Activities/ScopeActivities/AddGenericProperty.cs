9f7f2de3a622fd14f1e8d063e9aaab20c1e6e601
Andrzej Kniola <hagunadari@gmail.com>
2019-02-17 20:12:50 +0100

    First commit for scope activities, let's call it alpha V1

---
+++ Kalladystine.DataModels/Kalladystine.DataModels.Generators.Activities/ScopeActivities/AddGenericProperty.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Activities;
+using Kalladystine.DataModels.Generators.Models;
+using System.ComponentModel;
+
+namespace Kalladystine.DataModels.Generators.Activities
+{
+    public sealed class AddGenericProperty<T> : NativeActivity
+    {
+        [RequiredArgument]
+        [Category("New property model")]
+        public InArgument<string> PropertyName { get; set; }
+
+        [Category("New property model")]
+        public InArgument<bool> RequiresInitialization { get; set; }
+
+        [Category("New property model")]
+        public InArgument<bool> MarkRequiredInBuilder { get; set; }
+
+        protected override void Execute(NativeActivityContext context)
+        {
+            var prModel = new PropertyModel(PropertyName.Get(context), typeof(T), RequiresInitialization.Get(context), MarkRequiredInBuilder.Get(context));
+            ((ClassModel)context.Properties.Find("ClassModel")).AddProperty(prModel);
+        }
+    }
+}
---
+++ Kalladystine.DataModels/Kalladystine.DataModels.Generators.Activities/ScopeActivities/AddProperty.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Activities;
+using Kalladystine.DataModels.Generators.Models;
+using System.ComponentModel;
+
+namespace Kalladystine.DataModels.Generators.Activities
+{
+    public sealed class AddProperty : NativeActivity
+    {
+        [RequiredArgument]
+        [Category("New property model")]
+        public InArgument<string> PropertyName { get; set; }
+        [RequiredArgument]
+        [Category("New property model")]
+        public InArgument<string> PropertyTypeName { get; set; }
+        [Category("New property model")]
+        public InArgument<bool> RequiresInitialization { get; set; }
+        [Category("New property model")]
+        public InArgument<bool> MarkRequiredInBuilder { get; set; }
+
+        protected override void Execute(NativeActivityContext context)
+        {
+            
+            var prModel = new PropertyModel(PropertyName.Get(context), PropertyTypeName.Get(context), RequiresInitialization.Get(context), MarkRequiredInBuilder.Get(context));
+            ((ClassModel)context.Properties.Find("ClassModel")).AddProperty(prModel);
+
+        }
+    }
+}
